938f950bbe5efbe5151bfa3c9471b666b4fc6c0c
MorgunovDmitriy <137083703+MorgunovDmitriy@users.noreply.github.com>
2023-08-22 22:34:48 +0600

    first commit

---
+++ games/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from .models import *
+
+
+admin.site.register(Game)
+
+admin.site.register(Genre)
+
+
+class GameInline(admin.TabularInline):
+    model = Game
+    extra = 1
+
+
+@admin.register(Studio)
+class StudioAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'workers_count']
+    inlines = [GameInline]
---
+++ usersapp/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+
+class Player(models.Model):
+    nick = models.CharField(max_length=55)
+    
