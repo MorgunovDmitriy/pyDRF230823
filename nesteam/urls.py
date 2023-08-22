3c10d96fa8465196cbd29f64b71fa7210ae004b2
MorgunovDmitriy <137083703+MorgunovDmitriy@users.noreply.github.com>
2023-08-22 23:36:05 +0600

    Simple JWT

--- nesteam/urls.py
+++ nesteam/urls.py
@@ -18,10 +18,18 @@ from django.contrib import admin
 from django.urls import path, include
 from games.views import *
 from rest_framework import routers
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 
 router = routers.DefaultRouter()
 router.register(r'genre', GenreViewSet)
+
+
+
 
 
 urlpatterns = [
@@ -36,6 +44,9 @@ urlpatterns = [
     # path('auth/', include('djoser.urls.authtoken')),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('users/', include('usersapp.urls')),
     path('collections/', include('collection.urls')),
     path('', include(router.urls)),
